b4658e75a02698180ba9c250f594a13a3ea1bdd1
Jordan Jones <jordpjones@gmail.com>
2021-05-23 17:15:35 -0700

    Init

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-EternalRealmsMC
+# EternalRealmsMC
+
+### See more: https://eternalrealms.net/wiki/public-api/
+
+Usage:
+```javascript
+const EternalRealmsClient = require('eternalrealmsmc');
+const Client = new EternalRealmsClient.Client();
+```
+
+Example:
+
+```javascript
+const EternalRealmsClient = require('eternalrealmsmc');
+const Client = new EternalRealmsClient.Client();
+
+Client.online()
+```
+
+Result:
+```
+{
+    "survival": [
+        {
+            "group": 4,
+            "name": "Kashall",
+            "user_id": 19,
+            "start": 1621810191
+        }
+    ],
+    "stage": [],
+    "dev": []
+}
+```
---
+++ src/Client.js
@@ -0,0 +1,59 @@
+const fetch = require('node-fetch');
+
+/**
+ * Client for Eternal Realms MC 
+ * https://eternalrealms.net
+ */
+
+class EternalRealmsMCClient {
+
+    /**
+     * @typedef {Object} EternalRealmsMCClientOptions
+     * @param {String} [url] Base URL for Eternal Realms Client
+     * @memberof EternalRealmsMCClient
+     */
+
+    /**
+     * @param {EternalRealmsMCClientOptions} [options] Client Options
+     */
+    constructor(options = {}) {
+
+        /**
+         * Client Options
+         * @type {Object}
+         */
+        this.options = options;
+
+        /**
+        * Base URL 
+        * @type {String}
+        */
+        this.baseURL = options.url || "https://api.eternal.gs/"
+    }
+
+    online(server) {
+        return this._get(`online${server ? `/${server}` : ''}`)
+    }
+
+    user(user) {
+        return this._get(`user${user ? `/${user}` : ''}`)
+    }
+
+    staff() {
+        return this._get(`online/staff`)
+    }
+
+    stats() {
+        return this._get('stats')
+    }
+
+    servers() {
+        return this._get('servers')
+    }
+
+    _get(endpoint) {
+        const request = fetch(`${this.baseURL}${endpoint}`)
+            .then(res => res.json())
+            .then(json)
+    }
+}
